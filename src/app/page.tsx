0e0705b9f9164ecfae2f3df96dacbdc739ac4e14
natharika <natharika.tba@gmail.com>
2024-11-04 19:50:41 +0700

    Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,9 @@
-import { Button } from "@yuemnoi/components/ui/button";
-import { Input } from "@yuemnoi/components/ui/input";
-import { Tabs, TabsList, TabsTrigger } from "@yuemnoi/components/ui/tabs";
-
 export default function Home() {
   return (
     <div>
-      ลองลอง
-      <h1 className="h3 text-primary font-bold">hello</h1>
-      <Button>button</Button>
-      <Button variant="outline">outline</Button>
-      <Tabs defaultValue="account">
-        <TabsList>
-          <TabsTrigger value="account">Account</TabsTrigger>
-          <TabsTrigger value="password">เครื่องคิดเลข</TabsTrigger>
-        </TabsList>
-      </Tabs>
-      <Input placeholder="test" />
+      <h1 className="w-full my-20 font-bold text-black text-4xl text-center">
+        YEUM NOI
+      </h1>
     </div>
   );
 }
